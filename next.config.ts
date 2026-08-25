11a3a406d8122b167e5f1bde3d132931b353424e
in7md <hamadalbsrh@gmail.com>
2026-08-25 13:02:56 +0300

    Remove standalone output for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: process.env.STANDALONE === 'true' ? 'standalone' : undefined,
   /* config options here */
 };
 
